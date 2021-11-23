6955dff15350075459d99352cbe382adc2e0658c
James Jefferson <jrj72@Jamess-Mac-mini.local>
2021-11-22 22:53:15 -0600

    firebase setup

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ import App from './App';
 
 ReactDOM.render( <App />, document.getElementById('root'));
 
+// client side rendered app: (cra)
+  // -> database which is firebase
+  // -> react-loading- skeleton
+  // -> Tailwind
+
+// architecture
+  // -> components, constants, context, helpers, lib 
+  // (firebase is going to live in here), services(firebase functions in here)
+  //-> STYLE (TAILwind)
