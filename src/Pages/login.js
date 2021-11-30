693c2896f9f5e5e0108f05f98c17c2ba4b9e10f1
James Jefferson <jrj72@Jamess-Mac-mini.local>
2021-11-29 23:13:41 -0600

    Settin up login function

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,11 +1,22 @@
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router";
 import FirebaseContext from "../Context/firebase";
 
 export default function Login() {
-    const history = useNavigate();
-    const { firebase } = useContext(FirebaseContext);
+  const history = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
 
-    return(<p> I am the Login Page</p>)
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
 
-};
+  const [ error, setError ] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+      document.title = 'Login - Instagram';
+      }, []);
+
+  return <p> I am the Login Page</p>;
+}
